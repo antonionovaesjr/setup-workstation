d566c27b46933c636b60d0a5c7908f36160b4ff6
antonionovaesjr <antonionovaesjr@gmail.com>
2020-04-24 18:57:05 -0400

    Update setup.sh

--- opensuse-15.1/setup.sh
+++ opensuse-15.1/setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-zypper addrepo https://download.opensuse.org/repositories/home:jfita:Virtualization/openSUSE_Leap_15.1/home:jfita:Virtualization.repo
 zypper addrepo https://download.opensuse.org/repositories/home:olh/openSUSE_15.1/home:olh.repo
 
 zypper refresh
 
-zypper install libwebkitgtk-1_0-0
+zypper install libwebkitgtk-1_0-0 -y
 
 
-zypper install git ansible docker docker-compose libreoffice libreoffice-l10n-pt_BR cups keepassxc cockpit -y
-systemctl enable cockpit
-systemctl start cockpit
+zypper install git ansible docker docker-compose libreoffice libreoffice-l10n-pt_BR cups keepassxc -y
 
 systemctl enable docker
 systemc start docker
@@ -22,6 +19,11 @@ docker pull sonatype/nexus3:3.22.1
 docker pull owasp/zap2docker-stable:2.9.0
 docker pull portainer/portainer:1.23.2
 
+zypper install code-1.44.2-1587059974.el7.x86_64.rpm ICAClient-suse-13.8.0.10299729-0.x86_64.rpm  -y
 
+zypper install google-chrome-stable_current_x86_64.rpm teams-1.3.00.5153-1.x86_64.rpm -y 
 
-zypper install code-1.44.2-1587059974.el7.x86_64.rpm ICAClient-suse-13.8.0.10299729-0.x86_64.rpm google-chrome-stable_current_x86_64.rpm teams-1.3.00.5153-1.x86_64.rpm teamviewer-suse_15.4.4445.x86_64.rpm zoom_openSUSE_x86_64.rpm -7y
+zypper install teamviewer-suse_15.4.4445.x86_64.rpm zoom_openSUSE_x86_64.rpm -y
+
+mv /opt/Citrix/ICAClient/keystore/cacerts /opt/Citrix/ICAClient/keystore/cacerts-backup
+ln -s /etc/ssl/certs /opt/Citrix/ICAClient/keystore/cacerts
